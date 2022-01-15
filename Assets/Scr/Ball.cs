d2e91fe6f9f3f3e63f1ff7a4e4fe41e5338f0b15
ana-os-mo <anmosoriomo@unal.edu.co>
2022-01-14 19:05:25 -0500

    breakable block tiles

--- Assets/Scr/Ball.cs
+++ Assets/Scr/Ball.cs
@@ -4,42 +4,41 @@ using Random = UnityEngine.Random;
 public class Ball : MonoBehaviour
 {
     private const float BALL_VELOCITY_MIN_AXIS_VALUE = 0.5f;
-
+    
     [SerializeField]
     private float _initSpeed = 5;
     [SerializeField]
     private float _minSpeed = 4;
     [SerializeField]
     private float _maxSpeed = 7;
-
+    
     private Rigidbody2D _rb;
     private Collider2D _collider;
 
-    void Start()
+    public void Start()
     {
         Init();
     }
 
-    void Init()
+    public void Init()
     {
         _rb = GetComponent<Rigidbody2D>();
         _collider = GetComponent<Collider2D>();
-
+        
         _collider.enabled = true;
         _rb.velocity = Random.insideUnitCircle.normalized * _initSpeed;
-        // _rb.velocity = Vector2.right;
     }
-
+    
     void FixedUpdate()
     {
         CheckVelocity();
     }
-
+    
     private void CheckVelocity()
     {
         Vector2 velocity = _rb.velocity;
         float currentSpeed = velocity.magnitude;
-
+        
         if (currentSpeed < _minSpeed)
         {
             velocity = velocity.normalized * _minSpeed;
@@ -48,16 +47,31 @@ public class Ball : MonoBehaviour
         {
             velocity = velocity.normalized * _maxSpeed;
         }
-
-        if (Mathf.Abs(velocity.x) < BALL_VELOCITY_MIN_AXIS_VALUE)
+        
+        if(Mathf.Abs(velocity.x) < BALL_VELOCITY_MIN_AXIS_VALUE) 
         {
-            velocity.x += Mathf.Sign(velocity.x) * BALL_VELOCITY_MIN_AXIS_VALUE * Time.deltaTime;
+            float sign = velocity.x == 0 ? Mathf.Sign(-transform.position.x) : Mathf.Sign(velocity.x);
+            velocity.x += sign * BALL_VELOCITY_MIN_AXIS_VALUE * Time.deltaTime;
         }
         else if (Mathf.Abs(velocity.y) < BALL_VELOCITY_MIN_AXIS_VALUE)
         {
-            velocity.y += Mathf.Sign(velocity.y) * BALL_VELOCITY_MIN_AXIS_VALUE * Time.deltaTime;
+            float sign = velocity.y == 0 ? Mathf.Sign(-transform.position.y) : Mathf.Sign(velocity.y);   
+            velocity.y += sign * BALL_VELOCITY_MIN_AXIS_VALUE * Time.deltaTime;
         }
 
         _rb.velocity = velocity;
+    }
+    
+    // Esta funcion es invocada por Unity cuando se produce una colision
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        BlockTile blockTileHit;
+        if (!other.collider.TryGetComponent(out blockTileHit))
+        {
+            return;
+        }
+
+        ContactPoint2D contactPoint = other.contacts[0];
+        blockTileHit.OnHitCollision(contactPoint);
     }
 }
---
+++ Assets/Scr/Grid/GridController.cs
@@ -0,0 +1,75 @@
+using UnityEngine;
+
+public class GridController : MonoBehaviour
+{
+    [SerializeField]
+    private Vector2 _offset = new Vector2(-5.45f, 4);
+    // [SerializeField]
+    // private LevelData _currentLevelData;
+
+    private void Start()
+    {
+        BuildGrid();
+    }
+
+    private void BuildGrid()
+    {
+        int rowCount = 6;
+        int blockCount = 7;
+        float verticalSpacing = 0.1f;
+        float horizontalSpacing = 0.1f;
+        // BlockColor blockColor = rowData.BlockColor;
+        Vector2 blockSize = new Vector2(1.5f, 0.5f);
+        BlockTile blockTilePrefab = Resources.Load<BlockTile>("Prefabs/BigBlockTile");
+
+        // int rowCount = _currentLevelData.RowCount;
+        // float verticalSpacing = _currentLevelData.rowSpacing;
+
+        for (int j = 0; j < rowCount; j++)
+        {
+        //     GridRowData rowData = _currentLevelData.Rows[j];
+            
+        //     int blockCount = rowData.BlockAmount;
+        //     float horizontalSpacing = rowData.blockSpacing;
+        //     Vector2 blockSize = GetBlockSize(rowData.BlockType);
+        //     BlockTile blockTilePrefab = Resources.Load<BlockTile>(GetBlockPath(rowData.BlockType));
+        //     BlockColor blockColor = rowData.BlockColor;
+
+        //     if (blockTilePrefab == null)
+        //     {
+        //         return;
+        //     }
+            
+            for (int i = 0; i < blockCount; i++)
+            {
+                BlockTile blockTile = Instantiate<BlockTile>(blockTilePrefab, transform);
+                float x = _offset.x + blockSize.x/2 + (blockSize.x + horizontalSpacing) * i;
+                float y = _offset.y - (blockSize.y + verticalSpacing) * j;
+                blockTile.transform.position = new Vector3(x, y, 0);
+                
+                // blockTile.SetData(blockColor);
+                blockTile.Init();
+            }
+        }
+    }
+
+    // private Vector2 GetBlockSize(BlockType type)
+    // {
+    //     if (type == BlockType.Big)
+    //     {
+    //         return new Vector2(1.5f, 0.5f);
+    //     }
+
+    //     return Vector2.zero;
+    // }
+
+    // private string GetBlockPath(BlockType type)
+    // {
+    //     if (type == BlockType.Big)
+    //     {
+    //         return "Prefabs/BigBlockTile";
+    //     }
+
+    //     return string.Empty;
+    // }
+}
