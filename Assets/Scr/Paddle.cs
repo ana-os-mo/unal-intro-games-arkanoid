2551a3177f2d5b5bc89b6d9c6a674e7daf6051ea
Ana Osorio <88041760+ana-os-mo@users.noreply.github.com>
2022-01-15 07:18:44 -0500

    Power ups (#9)
    * PowerUps are randomly activated by destroying tiles - not yet functional
    
    * Large-Small paddle powerUp

--- Assets/Scr/Paddle.cs
+++ Assets/Scr/Paddle.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class Paddle : MonoBehaviour
 {
     [SerializeField]
-    private float _speed = 5;
+    private float _speed = 7;
     [SerializeField]
     private float _movementLimit = 7;
 
@@ -29,5 +29,17 @@ public class Paddle : MonoBehaviour
         _targetPosition.y = this.transform.position.y;
 
         transform.position = Vector3.Lerp(transform.position, _targetPosition, Time.deltaTime * _speed);
+    }
+
+    public void ScalePaddle(PowerUpKind kind)
+    {   
+        if (kind == PowerUpKind.LargePaddle)
+        {   
+            this.transform.localScale += new Vector3(0.3f, 0.0f);
+        }
+        else if (kind == PowerUpKind.SmallPaddle)
+        {
+            this.transform.localScale -= new Vector3(0.3f, 0.0f);
+        }
     }
 }
